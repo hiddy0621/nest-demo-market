01885d566aa432912fe2f5e36c7ebdcd1ae12ae7
hidenao-otake <h0i6d2e1@gmail.com>
2023-03-08 10:22:56 +0900

    Create Entity / Item Column on DB

---
+++ ormconfig.js
@@ -0,0 +1,17 @@
+module.exports = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'postgres',
+  autoLoadEntities: true,
+  // コンパイル後のマイグレーションファイルの読み込み指定
+  entities: ['dist/entities/**.entity.js'],
+  migrations: ['dist/migrations/**.js'],
+  //　CLIによって EntityファイルやMigrationファイルが出力された時の指定
+  cli: {
+    entitiesDir: 'src/entities',
+    migrationsDir: 'src/migrations',
+  },
+};
